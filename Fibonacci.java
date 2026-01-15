18043525f23a50a02674cdfa6cfc09148cafce27
KrishBawangade <krishbawangade08@gmail.com>
2026-01-15 07:29:13 +0530

    Fibonacci Program

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+
+    static int fibonacci(int n){
+        if(n<2){
+            return n;
+        }
+
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String[] args){
+        System.out.println(fibonacci(9));
+    }
+}
