c23caee91ea9f47cec0a37340c0a61ec654890c7
KrishBawangade <krishbawangade08@gmail.com>
2026-02-05 05:45:34 +0530

    Generate Subsequences for a given string Program

---
+++ strings/SubSeq.java
@@ -0,0 +1,30 @@
+package strings;
+
+import java.util.ArrayList;
+
+public class SubSeq{
+
+    static ArrayList<String> subSequences(String originalStr, String subSeqStr){
+        ArrayList<String> arr = new ArrayList<>();
+        if(originalStr.isEmpty()){
+            if(!subSeqStr.isEmpty()){
+                // return the subsequence if we reached a point where original str becomes empty
+                // And avoid empty subsequence
+                arr.add(subSeqStr);
+            }
+            return arr;
+        }
+        // add all elements returned from left side of recursion
+        arr.addAll(subSequences(originalStr.substring(1), subSeqStr+originalStr.charAt(0)));
+
+        // add all elements returned from right side of recursion
+        arr.addAll(subSequences(originalStr.substring(1), subSeqStr));
+
+        return arr;
+    } 
+
+    public static void main(String[] args){
+        String s1 = "abc";
+        System.out.println(subSequences(s1, ""));
+    }
+}
